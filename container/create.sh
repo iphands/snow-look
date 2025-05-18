6b4acf59548f832973597b10a1d95336519f24af
Ian Page Hands <iphands@gmail.com>
2025-05-18 15:20:14 -0700

    snow-look: create should rm old

--- container/create.sh
+++ container/create.sh
@@ -2,6 +2,11 @@
 source /main/docker/snow-look/spec
 cd /main/docker/snow-look/spec
 
+docker stop snow-look
+docker rm snow-look
+
+bash ./build.sh
+
 docker create --name $NAME \
   -v /main/docker/snow-look/data:/src/data
   $NAME
